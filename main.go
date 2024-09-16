118d7f6f5b0c9be7a627051b0151dcb01ecc8720
andsel <selva.andre@gmail.com>
2024-09-16 12:41:51 +0200

    Update the IP 216.160.83.58 with an integer 'is_in_european_union' attribute

--- main.go
+++ main.go
@@ -12,60 +12,34 @@ import (
 
 func main() {
 	// Load the database we wish to enrich.
-	writer, err := mmdbwriter.Load("GeoLite2-Country.mmdb", mmdbwriter.Options{})
+	writer, err := mmdbwriter.Load("/path/to/logstash_plugins/logstash-filter-geoip/src/test/resources/maxmind-test-data/GeoIP2-Country-Test.mmdb", mmdbwriter.Options{})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Define and insert the new data.
-	_, sreNet, err := net.ParseCIDR("56.0.0.0/16")
+    // Modify existing IP
+	_, singleNet, err := net.ParseCIDR("216.160.83.58/32")
 	if err != nil {
 		log.Fatal(err)
 	}
-	sreData := mmdbtype.Map{
-		"AcmeCorp.DeptName": mmdbtype.String("SRE"),
-		"AcmeCorp.Environments": mmdbtype.Slice{
-			mmdbtype.String("development"),
-			mmdbtype.String("staging"),
-			mmdbtype.String("production"),
+	singleHostData := mmdbtype.Map{
+		//"is_in_european_union": mmdbtype.Uint16(1),
+		"country": mmdbtype.Map{
+			"geoname_id": mmdbtype.Uint64(2750405),
+			"is_in_european_union": mmdbtype.Uint16(1),
+			"iso_code": mmdbtype.String("NL"),
+			"names": mmdbtype.Map{
+				"en": mmdbtype.String("Netherlands"),
+			},
 		},
 	}
-	if err := writer.InsertFunc(sreNet, inserter.TopLevelMergeWith(sreData)); err != nil {
+	if err := writer.InsertFunc(singleNet, inserter.TopLevelMergeWith(singleHostData)); err != nil {
 		log.Fatal(err)
 	}
 
-	_, devNet, err := net.ParseCIDR("56.1.0.0/16")
-	if err != nil {
-		log.Fatal(err)
-	}
-	devData := mmdbtype.Map{
-		"AcmeCorp.DeptName": mmdbtype.String("Development"),
-		"AcmeCorp.Environments": mmdbtype.Slice{
-			mmdbtype.String("development"),
-			mmdbtype.String("staging"),
-		},
-	}
-	if err := writer.InsertFunc(devNet, inserter.TopLevelMergeWith(devData)); err != nil {
-		log.Fatal(err)
-	}
-
-	_, mgmtNet, err := net.ParseCIDR("56.2.0.0/16")
-	if err != nil {
-		log.Fatal(err)
-	}
-	mgmtData := mmdbtype.Map{
-		"AcmeCorp.DeptName": mmdbtype.String("Management"),
-		"AcmeCorp.Environments": mmdbtype.Slice{
-			mmdbtype.String("development"),
-			mmdbtype.String("staging"),
-		},
-	}
-	if err := writer.InsertFunc(mgmtNet, inserter.TopLevelMergeWith(mgmtData)); err != nil {
-		log.Fatal(err)
-	}
 
 	// Write the newly enriched DB to the filesystem.
-	fh, err := os.Create("GeoLite2-Country-with-Department-Data.mmdb")
+	fh, err := os.Create("/path/to/logstash_plugins/logstash-filter-geoip/src/test/resources/maxmind-test-data/GeoIP2-Country-Test.mmdb")
 	if err != nil {
 		log.Fatal(err)
 	}
